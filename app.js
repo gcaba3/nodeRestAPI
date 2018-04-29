2414d9de5a1e0649c12e402d9dab9d945eb40ce1
gcaba3 <glynn.caballero@connect.qut.edu.au>
2018-04-29 23:00:10 +1000

    Connected to mongo

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://localhost:27017/mydb');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
